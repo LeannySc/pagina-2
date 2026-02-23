c95bec8236767ac9a2bfbfabe9fbd7781e7e0f50
leannyS <leannystandioy2003@gmail.com>
2026-02-23 14:44:13 -0500

    arreglo para visualizar en git hub

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // ¡ESTO ES LO MÁS IMPORTANTE! Crea archivos HTML/CSS puros
+  basePath: "/fundacion-edubilingue", // Pon exactamente el nombre de tu repositorio en GitHub
+  images: {
+    unoptimized: true, // GitHub Pages no puede optimizar imágenes automáticamente
+  },
 };
 
 export default nextConfig;
